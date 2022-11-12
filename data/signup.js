3fa31e159c8a016230ae9fed061e07d06f1882a4
Satyajeet <satyajeet.s001@webreinvent.com>
2022-11-12 14:22:42 +0530

    added test script

--- data/signup.js
+++ data/signup.js
@@ -28,9 +28,20 @@ class Signup{
                         name: "Verify heading of the Sign Up form.",
                         expect: "Heading of the sign up form should be present.",
                         assert : "Sign up to your account"
-
-
-                    }
+                    },
+                    {
+                        count: 1.2,
+                        name: "Verify user is able to signup by leaving first name text-field blank",
+                        expect: "User should not be able to signup and an error message should be displayed",
+                        first_name: "",
+                        last_name : "Test",
+                        email : "demo@gmail.com",
+                        password: "testing",
+                        assert: {
+                            error_msg: " Please fill out this field. ",
+                            sign_up_page_heading: "Sign up to your account"
+                        }
+                    },
                     ]
             }
             ]
